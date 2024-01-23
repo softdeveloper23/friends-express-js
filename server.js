fb644e7a136df015698dfd44650d078eee563398
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-23 19:00:11 +0100

    Added hbs template

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const messagesRouter = require('./routes/messages.router.js');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -22,6 +25,12 @@ app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // Set endpoint for friends and messages
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        title: 'My Friends Are VERY Clever!',
+        caption: 'Let\'s go skiing!!!',
+    });
+});
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 
