32ea4457ba79806c9e5d92f92da9596c60bfa10d
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-23 19:10:10 +0100

    Added a layout template

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
+    res.render('messages', {
+        title: 'Message to my Friends!',
+        friend: 'Gandalf the Grey',
+    });
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
 }
 
 function postMessages(req, res) {
