0acee15c4818cfa9921f02041b4080873ce0d981
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-23 13:17:54 +0100

    Added middleware to console.log method and url

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const friends = [
     },
 ];
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
 app.get('/friends', (req, res) => {
     res.json(friends);
 });
